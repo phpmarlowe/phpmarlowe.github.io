4e52d356eb014b9d51e40f13bd9ca498436a8312
hbsj <1548031817@qq.com>
2025-03-08 00:21:36 +0800

    test

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -10,7 +10,7 @@ export default defineThemeConfig({
 
   appearance: true, // 配置 深色模式
 
-  social: [{ icon: "qq", link: "/" }],
+  social: [{ icon: "qq", link: "http://wpa.qq.com/msgrd?v=3&uin=1548031817&site=qq&menu=yes" }],
   // navbarSocialInclude: ['github'], // 允许显示在导航栏的 social 社交链接
   // aside: true, // 页内侧边栏， 默认显示在右侧
   // outline: [2, 3], // 页内大纲， 默认显示 h2, h3
