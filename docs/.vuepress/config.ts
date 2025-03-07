5792302ef56505dbbff8df8bdd6cd0aae03c256b
hbsj <1548031817@qq.com>
2025-03-07 23:54:11 +0800

    test

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import { plumeTheme } from "vuepress-theme-plume";
 
 export default defineUserConfig({
-  base: "/phpmarlowe/",
+  base: "/",
   lang: "zh-CN",
   title: "phpmarlowe",
   description: "",
