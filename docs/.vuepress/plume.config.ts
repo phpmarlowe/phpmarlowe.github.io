b78fd0e20faa5f24320cecd364392e5a34a8af2b
hbsj <1548031817@qq.com>
2025-03-09 00:02:44 +0800

    1

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -6,11 +6,16 @@ import { notes } from "./notes";
  * @see https://theme-plume.vuejs.press/config/basic/
  */
 export default defineThemeConfig({
-  logo: "https://theme-plume.vuejs.press/plume.png",
+  logo: "/image.png",
 
   appearance: true, // 配置 深色模式
 
-  social: [{ icon: "qq", link: "http://wpa.qq.com/msgrd?v=3&uin=1548031817&site=qq&menu=yes" }],
+  social: [
+    {
+      icon: "qq",
+      link: "http://wpa.qq.com/msgrd?v=3&uin=1548031817&site=qq&menu=yes",
+    },
+  ],
   // navbarSocialInclude: ['github'], // 允许显示在导航栏的 social 社交链接
   // aside: true, // 页内侧边栏， 默认显示在右侧
   // outline: [2, 3], // 页内大纲， 默认显示 h2, h3
@@ -32,12 +37,13 @@ export default defineThemeConfig({
    * @see https://theme-plume.vuejs.press/config/basic/#profile
    */
   profile: {
-    avatar: "https://theme-plume.vuejs.press/plume.png",
-    name: "phpmarlowe",
+    avatar: "/gg02.jpg",
+    name: "guagua",
     description: "haha",
     circle: true,
     location: "",
     organization: "",
+    layout: "left",
   },
 
   navbar,
--- docs/README.md
+++ docs/README.md
@@ -4,12 +4,12 @@ config:
   - type: banner
     banner: https://api.pengzhanbo.cn/wallpaper/bing
     bannerMask:
-      light: 0.1
-      dark: 0.3
+      light: 0.01
+      dark: 0.7
     hero:
-      name: 瓜瓜和lmの爹
-      tagline: lqt
-      text: 加油努力拉屎要用力。
+      name: 这是一个博客 demo
+      tagline: phpmarlowe
+      text: 你好
       actions:
         - text: 我的博客
           link: /blog/
