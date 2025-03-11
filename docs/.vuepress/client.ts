3d23172399841ccb4606e9a760b2b6541495f396
liqingtai <qingtai.li@gable-tech.com.cn>
2025-03-11 16:23:24 +0800

    feat: 中文字体包过大问题

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -7,6 +7,7 @@ import { defineClientConfig } from "vuepress/client";
 // import CustomComponent from './theme/components/Custom.vue'
 
 import "./theme/styles/custom.css";
+import "./theme/styles/font.css";
 
 export default defineClientConfig({
   enhance({ app }) {
--- docs/README.md
+++ docs/README.md
@@ -15,3 +15,10 @@ config:
           text: Github
           link: https://github.com/pengzhanbo/vuepress-theme-plume
 ---
+
+<!--
+自定义域名编译后失效需重新手动配置？
+--CNAME 文件不是放在库的根目录，而是项目编译后网站的根目录下
+字体文件不生效？
+.ttf 太大 转换为 .woff2 格式
+ -->
